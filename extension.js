d169d7bfdec6f036c611fe809007463a241c3bf3
RosGtHub <89165766+RosGtHub@users.noreply.github.com>
2023-12-09 12:09:36 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,24 +1,44 @@
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';  
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 function onOverviewActivated() {
-    Main.overview.dash.hide();
+  Main.overview.dash.hide();
 }
 
 function onOverviewDeactivated() {
-    Main.overview.dash.show();
+  Main.overview.dash.show();
 }
 
-export default class  DockExtension{
-    enable() { 
-        this.showingSignalId = Main.overview.connect('showing', onOverviewActivated);
-        this.hiddenSignalId = Main.overview.connect('hiding', onOverviewDeactivated);
-    }
-    disable() {
-        Main.overview.disconnect(this.showingSignalId);
-        Main.overview.disconnect(this.hiddenSignalId);
-    }
-}
+export default class DockExtension {
+  constructor() {
+    this.showingSignalId = null;
+    this.hiddenSignalId = null;
+  }
 
-function init() {
-    return new DockExtension(); 
+  enable() {
+    if (this.showingSignalId) {
+      Main.overview.disconnect(this.showingSignalId);
+    }
+    if (this.hiddenSignalId) {
+      Main.overview.disconnect(this.hiddenSignalId);
+    }
+    this.showingSignalId = Main.overview.connect('showing', onOverviewActivated);
+    this.hiddenSignalId = Main.overview.connect('hiding', onOverviewDeactivated);
+  }
+
+  disable() {
+    if (this.showingSignalId) {
+      Main.overview.disconnect(this.showingSignalId);
+      this.showingSignalId = null;
+    }
+    if (this.hiddenSignalId) {
+      Main.overview.disconnect(this.hiddenSignalId);
+      this.hiddenSignalId = null;
+    }
+  }
+
+  static init() {
+    const extension = new DockExtension();
+    extension.enable();
+    return extension;
+  }
 }
