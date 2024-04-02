dbe46e7c7ad2176982f9018e333d200594acbb53
RosGtHub <89165766+RosGtHub@users.noreply.github.com>
2024-04-02 11:43:01 +0200

    BUG FIXES

---
+++ extension.js
@@ -0,0 +1,46 @@
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+let delay = 200;
+let timeout = null;
+
+function onOverviewActivated() {
+  Main.overview.dash.hide();
+}
+
+function onOverviewDeactivated() {
+  timeout = setTimeout(() => { 
+    Main.overview.dash.show();
+  }, delay);
+}
+
+export default class DockExtension {
+  constructor() {
+    this.showingSignalId = null;
+    this.hiddenSignalId = null;
+  }
+
+  enable() {
+    if (this.showingSignalId) {
+      Main.overview.disconnect(this.showingSignalId);
+    }
+    if (this.hiddenSignalId) {
+      Main.overview.disconnect(this.hiddenSignalId);
+    }
+    this.showingSignalId = Main.overview.connect('showing', onOverviewActivated);
+    this.hiddenSignalId = Main.overview.connect('hiding', onOverviewDeactivated);
+  }
+
+  disable() {
+    if (this.showingSignalId) {
+      Main.overview.disconnect(this.showingSignalId);
+      this.showingSignalId = null;
+    }
+    if (this.hiddenSignalId) {
+      Main.overview.disconnect(this.hiddenSignalId);
+      this.hiddenSignalId = null;
+    }
+    if(timeout){
+      clearInterval(timeout);
+    }
+  }
+}
